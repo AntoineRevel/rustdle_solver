c346d8cbb71ddab422c48a306a7ce6ce448bd87f
Antoine Revel <antoine.revel@gmail.com>
2023-01-26 14:52:48 +0100

    V1 fonctionnelle

--- src/game.rs
+++ src/game.rs
@@ -1,133 +1,163 @@
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 static FILE_PATH_EN : &str = "data/words.txt";
 
-fn start_game() {
-
-    let mut words =Words::new(4, "en");
-
-    //Words::start(&mut words, reply);
-
-    //print!("{:?}\n",words.words);
-    //print!("{}",pos.len());
-
+pub fn start_game() {
+    Menu::new().start();
 }
 
+struct Menu {
+    word_length: i16,
+    language: String,
+}
 
-pub fn menu() {
-    //default
-    let mut word_length =5;
-
-    println!("Welcome to wordle solveur");
-    println!("1. Modify word length");
-    println!("2. Modify language");
-    println!("3. Exit the game\n");
-    println!("Press Enter to start the game.");
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let input = input.trim();
-
-    match input {
-        "" => println!("Starting the game"),
-        "1" => {println!("Modifying word length");
-                word_length=choose_word_length()},
-        "2" => println!("Modifying language"),
-        "3" => println!("Goodbye!"),
-        _ => println!("Invalid option"),
+impl Menu {
+    fn new() ->Menu{
+        Menu { word_length: 5, language: "en".to_string() }
     }
-}
 
-fn choose_word_length() -> i32 {
-    println!("Please enter the desired word length for the game (between 2 and 20):");
+    fn start(&mut self){
+        println!("Welcome to Wordle solveur");
+        self.menu();
+    }
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let input = input.trim();
+    fn menu(&mut self) {
+        println!("Let's play with {} letter words in English.",self.word_length);
+        println!("Press Enter to start the game.");
+        println!("1. Modify word length");
+        println!("2. Modify language");
+        println!("3. Exit the game");
+        print!("=> ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
 
-    match input.parse::<i32>() {
-        Ok(word_length) => {
-            if word_length < 2 || word_length > 20 {
-                println!("Invalid input. Please enter a number between 2 and 20.");
-                choose_word_length()
-            } else {
-                word_length
+        match input {
+            "" => { println!("Starting the game");
+                Game::new(self.word_length, self.language.clone()).start();
             }
-        },
-        Err(_) => {
-            println!("Invalid input. Please enter a number between 2 and 20.");
-            choose_word_length()
+            "1" => {println!("Modifying word length");
+                self.word_length=Menu::choose_word_length();
+                self.menu();},
+            "2" => {println!("Modifying language");
+                self.menu();},
+            "3" => println!("Goodbye!"),
+            _ => {println!("Invalid option");
+                self.menu();},
+        }
+    }
+    fn choose_word_length() -> i16 {
+        println!("Please enter the desired word length for the game (between 2 and 20):");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        match input.parse::<i16>() {
+            Ok(word_length) => {
+                if word_length < 2 || word_length > 20 {
+                    println!("Invalid input. Please enter a number between 2 and 20.");
+                    Menu::choose_word_length()
+                } else {
+                    word_length
+                }
+            },
+            Err(_) => {
+                println!("Invalid input. Please enter a number between 2 and 20.");
+                Menu::choose_word_length()
+            }
         }
     }
 }
 
-
-
-
-
-struct Words {
-    size: usize,
-    possiblities: Vec<Vec<i8>>,
-    language: String,
+struct Game {
+    size: i16,
+    possibilities: Vec<Vec<i8>>,
     words: Vec<String>,
-    remaining_words :Vec<String>
 }
 
-impl Words {
-    fn new(size: usize, language: &str) -> Words {
+impl Game {
+    fn new(size: i16, language: String) -> Game {
         let words = Self::import_words(&language.to_string(), size);
-        let remaining_words=words.clone();
-        let possiblities = generate_possibilities(size);
-        let language=language.to_string();
-        Words { size , possiblities, language , words,remaining_words }
+        let possibilities = Game::generate_possibilities(size as usize);
+        Game { size , possibilities, words}
     }
 
-    fn import_words(language: &String, size: usize) -> Vec<String> {
-        let path = std::path::Path::new(FILE_PATH_EN);
+    fn generate_possibilities(n: usize) -> Vec<Vec<i8>> {
+        let possibilities = vec![0, 1, 2];
+        if n == 0 {
+            return vec![vec![]];
+        }
+        let mut result = Vec::new();
+        for combination in Game::generate_possibilities(n - 1).iter() {
+            for possibility in possibilities.iter() {
+                let mut new_combination = combination.clone();
+                new_combination.push(*possibility as i8);
+                result.push(new_combination);
+            }
+        }
+        result
+    }
+
+    fn import_words(language: &String, size: i16) -> Vec<String> {
+        let path :&Path;
+        if language=="en" {
+            path = Path::new(FILE_PATH_EN);
+        } else{
+            path= Path::new(FILE_PATH_EN);
+        }
         let file = File::open(path).expect("Failed to open file");
         let reader = BufReader::new(file);
 
         reader
         .lines()
-        .map(|line| line.expect("Failed to read line"))
-        .filter(|word| word.len() == size)
+        .map(|line| line.expect("Failed to read line").to_uppercase())
+        .filter(|word| word.len() == size as usize)
         .collect()
     }
 
-    fn size(&self) -> usize {
-        self.size
+    fn start(&mut self){
+        self.start_first();
+        self.continue_game();
     }
 
-    fn words(&self) -> &Vec<String> {
-        &self.words
+    fn continue_game(&mut self){
+        let word=self.find_best();
+        print!("{}  --> {}\n",word.0,word.1);
+        let reply=self.input_sequence();
+        let word_color=Game::display_colored_text(word.0,&reply);
+        let words_len_before= self.words.len();
+        self.words= self.eliminate(word.0,reply);
+        if self.words.len()>1 {
+            println!("{}  --> {} words eliminate",word_color,words_len_before-self.words.len());
+            self.continue_game();
+        } else {
+            println!("The word is {}",self.words[0])
+        }
     }
 
-    fn count(&self) -> usize {
-        self.words.len()
+    fn start_first(&mut self){
+        let first_word="JUDAS".to_string(); // self.words.choose(&mut rand::thread_rng()).unwrap();
+        println!("Enter a sequence of {} numbers (only 0, 1, or 2): ", self.size);
+        println!("{}  --> first random proposition",first_word);
+
+        let first_reply=self.input_sequence();
+        let word_color=Game::display_colored_text(&first_word,&first_reply);
+        let words_len_before= self.words.len();
+        self.words= self.eliminate(&first_word,first_reply);
+        println!("{}  --> {} words eliminate",word_color,words_len_before-self.words.len());
     }
 
-    fn start(&mut self, first_reply: Reply){
-        let first_reply_sec=self.input_sequence();
-
-        println!("{:?}",first_reply_sec);
-        let word=self.find_best(first_reply);
-        print!("best word : {} with E(.)={} ",word.0,word.1);
-
-
-        //self.words=self.elimine(word.chars().collect(),inpu);
-
-    }
-
-    fn input_sequence(&self) -> Vec<i32> {
+    fn input_sequence(&self) -> Vec<i8> {
         let mut result = vec![];
         loop {
-            println!("Enter a sequence of {} numbers (only 0, 1, or 2): ", self.size);
             let mut sequence = String::new();
             io::stdin().read_line(&mut sequence).unwrap();
             let sequence = sequence.trim();
-            if sequence.len() != self.size {
+            if sequence.len() != self.size as usize {
                 println!("\x1B[31mInvalid input, the sequence must be of length {}\x1B[0m", self.size);
                 continue;
             }
@@ -158,31 +188,39 @@ impl Words {
         result
     }
 
+    fn display_colored_text(text: &String, colors: &Vec<i8>) -> String{
+        let mut output = String::new();
+        for (i, c) in text.chars().enumerate() {
+            match colors[i] {
+                0 => output.push_str(&format!("{}",c)),
+                1 => output.push_str(&format!("\x1b[33m{}\x1b[0m", c)),
+                2 => output.push_str(&format!("\x1b[32m{}\x1b[0m", c)),
+                _ => output.push(c),
+            }
+        }
+        output
+    }
 
-
-    fn find_best(&self, reply : Reply) -> (&String,i32){
-        let mut max_esperance=0;
+    fn find_best(&self) -> (&String, i32){
+        let mut max_esperance =0;
         let mut best_word:&String=&self.words[0];
         for _word in self.words.iter() {
-            let esperance=Self::compute_esperance(self,_word);
-            if esperance>max_esperance{
-                max_esperance=esperance;
+            let esperance =Self::compute_esperance(self, _word);
+            if esperance > max_esperance {
+                max_esperance = esperance;
                 best_word=_word;
             }
             //print!("E({})={}\n\n",_word,esperance);
         }
-        (best_word,max_esperance)
+        (best_word, max_esperance)
     }
 
     fn compute_esperance(&self, word : &String) -> i32{
-        let wordChar = word.chars().collect();
         let mut esperance=0;
         let len_words=self.words.len();
-        for _possiblities in self.possiblities.iter() {
-            let mots_restant=self.elimine(&wordChar,_possiblities.to_vec()).len();
+        for _possibles in self.possibilities.iter() {
+            let mots_restant=self.eliminate(word, _possibles.to_vec()).len();
             esperance+= mots_restant*(len_words-mots_restant);
-
-
             //print!("    {:?},{}\n",_possiblities,esperance);
 
         }
@@ -190,87 +228,47 @@ impl Words {
         esperance as i32
     }
 
-    fn elimine(&self,suggestion :&Vec<char>, reply: Vec<i8>) -> Vec<String>{
+    fn eliminate(&self, word :&String, reply: Vec<i8>) -> Vec<String>{
+        let word_separate=Game::separate_strings(word,&reply);
+
         let mut words_reply=self.words.clone();
+        println!("{}",Game::display_colored_text(word,&reply));
 
 
-        for(char_i,rep_i) in suggestion.iter().zip(reply.iter()){
-
-            //println!("      item1: {} item2: {}", char_i,rep_i);
-        }
-
-        let letres_presente = suggestion.into_iter()
-            .zip(reply.into_iter())
-            .filter(|(_, rep_i)| *rep_i == 1 || *rep_i == 2)
-            .map(|(char_i, _)| char_i)
-            .collect::<Vec<_>>();
-
-
-        //println!("      {:?}",letresPresente);
-        words_reply=words_reply.into_iter()
-            .filter(|word| {
-                letresPresente.iter().all(|letter| word.contains(letter.to_string().as_str()))
+        words_reply= words_reply.into_iter()
+                .filter(|word|
+                    word_separate.2.iter()
+                        .all(|(c, i)| word.chars().nth(*i) == Some(*c))
+                )
+                .filter(|word| {
+                let word_chars: Vec<(char, usize)> = word.chars().enumerate().map(|(i,c)|(c,i)).collect();
+                    word_separate.1.iter()
+                    .all(|(c, i)| !word_chars.contains(&(*c, *i))) && word_separate.1.iter().all(|(c, _)| word.contains(*c))
             })
-            .collect();
+            .filter(|word| {
+                word_separate.0.iter()
+                    .all(|c| !word.contains(*c))
+            })
+                .collect();
 
-        //println!("      {:?}",words_reply);
-
+        println!("{:?}\n{}",words_reply,words_reply.len());
         words_reply
     }
 
-
-}
-
-#[derive(Debug)]
-enum ReplyType {
-    Correct,
-    WrongSpot,
-    NotInTheWorld
-}
-struct Reply {
-    suggestion: Vec<char>,
-    reply : Vec<i8>
-}
-
-impl Reply {
-    fn new(suggestion :String, reply : Vec<i8>) -> Reply {
-        let size=reply.len();
-        if suggestion.len() != size {panic!("Error wrong len")}
-        Reply { suggestion: suggestion.chars().collect(), reply }
-    }
-
-}
-
-
-
-//     Correct       -> 2
-//     WrongSpot     -> 1
-//     NotInTheWorld -> 0
-
-fn generate_possibilities(n: usize) -> Vec<Vec<i8>> {
-    let possibilities = vec![0, 1, 2];
-    if n == 0 {
-        return vec![vec![]];
-    }
-    let mut result = Vec::new();
-    for combination in generate_possibilities(n - 1).iter() {
-        for possibility in possibilities.iter() {
-            let mut new_combination = combination.clone();
-            new_combination.push(*possibility as i8);
-            result.push(new_combination);
+    fn separate_strings(s: &String, v: &Vec<i8>) -> (Vec<char>, Vec<(char, usize)>, Vec<(char, usize)>) {
+        let mut l0 = vec![];
+        let mut l1 = vec![];
+        let mut l2 = vec![];
+        for (i, c) in s.chars().enumerate() {
+            match v[i] {
+                0 => l0.push(c),
+                1 => l1.push((c, i)),
+                2 => l2.push((c, i)),
+                _ => (),
+            }
         }
+        (l0, l1, l2)
     }
-    result
+
 }
-
-
-
-
-
-
-
-
-
-
-
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod game;
 
 fn main() {
-    game::menu();
+    game::start_game();
 
-    //println!("{:?}",generate_possibilities(3));
+
 }
